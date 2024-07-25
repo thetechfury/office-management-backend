619cf685459d03afd6e9d76790300d055aece103
Usama <usamabfdpc@gmail.com>
2024-07-25 11:00:23 +0500

    -add slash / at the end of endpoints of APIs

--- office/users/urls.py
+++ office/users/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     path("user/update-password/",views.UpdatePasswordAPI.as_view()),
     path("auth/login/",views.LoginAPI.as_view()),
     path("auth/logout/",views.LogoutAPI.as_view()),
-    path('user/<user_id>/profile',views.GetUserProfile.as_view()),
-    path('user/<user_id>/address',views.GetUserAddress.as_view()),
-    path('user/<user_id>/experience',views.GetUserExperience.as_view()),
-    path('user/<user_id>/education',views.GetUserEducation.as_view()),
-    path('user/<user_id>/skills',views.GetUserSkills.as_view()),
-    path('user/<user_id>/teams',views.GetUserTeams.as_view()),
+    path('user/<user_id>/profile/',views.GetUserProfile.as_view()),
+    path('user/<user_id>/address/',views.GetUserAddress.as_view()),
+    path('user/<user_id>/experience/',views.GetUserExperience.as_view()),
+    path('user/<user_id>/education/',views.GetUserEducation.as_view()),
+    path('user/<user_id>/skills/',views.GetUserSkills.as_view()),
+    path('user/<user_id>/teams/',views.GetUserTeams.as_view()),
     path("user/",include(user_router.urls)),
 
 ]
